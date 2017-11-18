2a92cd2a7d2f84c6ddac9f770b6d23fbaf1dc76d
Masato Deushi <m.chelsea817@gmail.com>
2017-11-18 20:38:32 +0900

    chapter5.9 title3

---
+++ ch5/title3/title3.go
@@ -0,0 +1,48 @@
+package title3
+
+import (
+	"golang.org/x/net/html"
+	"fmt"
+)
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			// no panic
+		case bailout{}:
+			err = fmt.Errorf("multiple title elements")
+		default:
+			panic(p)
+		}
+	}()
+
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{})
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+	if title == "" {
+		return "", fmt.Errorf("no titile element")
+	}
+	return title, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
