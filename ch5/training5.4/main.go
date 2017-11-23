58c5cb4ed76dc57067dfac49409954a1ae1e5ce4
Masato Deushi <m.chelsea817@gmail.com>
2017-11-23 17:03:54 +0900

    chapter5 training5.4

---
+++ ch5/training5.4/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "visit:%v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && (n.Data == "a" || n.Data == "link" || n.Data == "img" || n.Data == "script") {
+		for _, a := range n.Attr {
+			if a.Key == "href" || a.Key == "src" || a.Key == "link" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
