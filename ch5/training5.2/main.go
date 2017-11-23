5877948af641f129c80e4dd596cd27ed73d6ffec
Masato Deushi <m.chelsea817@gmail.com>
2017-11-23 16:32:48 +0900

    chapter5 training5.2

---
+++ ch5/training5.2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+var domCount = make(map[string]int)
+
+func count(n *html.Node) {
+	if n == nil {
+		return
+	}
+
+	if n.Type == html.ElementNode {
+		domCount[n.Data]++
+	}
+
+	count(n.FirstChild)
+	count(n.NextSibling)
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	count(doc)
+	for dom, num := range domCount {
+		fmt.Printf("%s: %d\n", dom, num)
+	}
+}
