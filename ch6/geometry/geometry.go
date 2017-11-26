ada26ddf31366f07c2468536da9f07010d49b24a
Masato Deushi <m.chelsea817@gmail.com>
2017-11-26 18:12:16 +0900

    chapter6 geometry

--- ch6/geometry/geometry.go
+++ ch6/geometry/geometry.go
@@ -13,3 +13,15 @@ func Distance(p, q Point) float64 {
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
+
+type Path []Point
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
