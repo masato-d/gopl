3fe6451d20e96947acf3bee35598784afe5f3d28
Masato Deushi <m.chelsea817@gmail.com>
2017-12-22 23:58:35 +0900

    chapter7 eval(in continue)

---
+++ ch7/evaluator/evaluator.go
@@ -0,0 +1,75 @@
+package evaluator
+
+import (
+	"fmt"
+	"math"
+)
+
+type Expr interface {
+	Eval(env Env) float64
+}
+
+type Env map[Var]float64
+
+type Var string
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
+type literal float64
+
+func (l literal) Eval(_ Env) float64 {
+	return float64(l)
+}
+
+type unary struct {
+	op rune // '+' or '-'
+	x  Expr
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+type binary struct {
+	op   rune // '+', '-', '*' or '/'
+	x, y Expr
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+type call struct {
+	fn   string // "pow", "sin" or "sqrt"
+	args []Expr
+}
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
+}
---
+++ ch7/evaluator/evaluator_test.go
@@ -0,0 +1,42 @@
+package evaluator
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/adonovan/gopl.io/ch7/eval"
+)
+
+func TestEval(t *testing.T) {
+	tests := []struct {
+		expr string
+		env  Env
+		want string
+	}{
+		{"sqrt(A / pi)", Env{"A": 87616, "pi": math.Pi}, "167"},
+		{"pow(x, 3) + pow(y, 3)", Env{"x": 12, "y": 1}, "1729"},
+		{"pow(x, 3) + pow(y, 3)", Env{"x": 9, "y": 10}, "1729"},
+		{"5 / 9 * (F - 32)", Env{"F": -40}, "-40"},
+		{"5 / 9 * (F - 32)", Env{"F": 32}, "32"},
+		{"5 / 9 * (F - 32)", Env{"F": 212}, "212"},
+	}
+
+	var prevExpr string
+	for _, test := range tests {
+		if test.expr != prevExpr {
+			fmt.Printf("\n%s\n", test.expr)
+			prevExpr = test.expr
+		}
+		expr, err := eval.Parse(test.expr)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		got := fmt.Sprintf("%.6g", expr.Eval(test.env))
+		fmt.Printf("\t%v => %s\n", test.env, got)
+		if got != test.want {
+			t.Errorf("%s.Eval() in %v = %q, want %q\n", test.expr, test.env, got, test.want)
+		}
+	}
+}
