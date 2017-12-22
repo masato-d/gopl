417f83312111d2c0a1f3e2f052efb5d35ece956e
Masato Deushi <m.chelsea817@gmail.com>
2017-12-23 02:00:15 +0900

    chapter7 evaluator, surface

--- ch7/evaluator/evaluator.go
+++ ch7/evaluator/evaluator.go
@@ -3,10 +3,12 @@ package evaluator
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 type Expr interface {
 	Eval(env Env) float64
+	Check(vars map[Var]bool) error
 }
 
 type Env map[Var]float64
@@ -17,10 +19,19 @@ func (v Var) Eval(env Env) float64 {
 	return env[v]
 }
 
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
 type literal float64
 
 func (l literal) Eval(_ Env) float64 {
 	return float64(l)
+}
+
+func (l literal) Check(vars map[Var]bool) error {
+	return nil
 }
 
 type unary struct {
@@ -36,6 +47,13 @@ func (u unary) Eval(env Env) float64 {
 		return -u.x.Eval(env)
 	}
 	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
 }
 
 type binary struct {
@@ -57,6 +75,16 @@ func (b binary) Eval(env Env) float64 {
 	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 }
 
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.e.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
 type call struct {
 	fn   string // "pow", "sin" or "sqrt"
 	args []Expr
@@ -73,3 +101,21 @@ func (c call) Eval(env Env) float64 {
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
