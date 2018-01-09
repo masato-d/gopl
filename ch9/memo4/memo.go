3eb92452efc0c26cd430aa9018bcbcd4a356e22e
Masato Deushi <m.chelsea817@gmail.com>
2018-01-10 02:00:28 +0900

    chapter8 memo

---
+++ ch9/memo4/memo.go
@@ -0,0 +1,57 @@
+package memo
+
+import (
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type entry struct {
+	res   result
+	ready chan struct{}
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]*entry)}
+}
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex
+	cache map[string]*entry
+}
+
+type Func func(key string) (interface{}, error)
+
+func (memo *Memo) Get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	e := memo.cache[key]
+	if e == nil {
+		e = &entry{ready: make(chan struct{})}
+		memo.cache[key] = e
+		memo.mu.Unlock()
+
+		e.res.value, e.res.err = memo.f(key)
+
+		close(e.ready)
+	} else {
+		memo.mu.Unlock()
+
+		<-e.ready
+	}
+	return e.res.value, e.res.err
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
