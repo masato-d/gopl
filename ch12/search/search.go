c516c39612c372f0843d2b5e029f87b7e40b95de
Masato Deushi <m.chelsea817@gmail.com>
2018-01-19 23:01:14 +0900

    chapter12 search

---
+++ ch12/search/search.go
@@ -0,0 +1,20 @@
+package search
+
+import (
+	"net/http"
+
+	"github.com/masato-d/gopl/ch12/params"
+)
+
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels     []string `http:"l"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}
+	data.MaxResults = 10
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
